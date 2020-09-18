e50d3e51cbc14f91ae2b2f4360717291227c1061
Alexander Gängel <alexander.gaengel@ionos.com>
2020-09-18 08:42:21 +0200

    verify tls of api endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ password=$(pwgen -s 30 1)
 
 branch=${GITHUB_REF/refs\/heads\//}
 
-username=$(http POST https://api.buildwith.ionos.com/v1/projects/$PROJECT/git-repository/branches/$branch/users password=$password Authorization:"API-Key $API_KEY" Content-Type:application/vnd.ionos.beat.buildwithionos-v1+json --verify=no --ignore-stdin | jq -r .username)
+username=$(http POST https://api.buildwith.ionos.com/v1/projects/$PROJECT/git-repository/branches/$branch/users password=$password Authorization:"API-Key $API_KEY" Content-Type:application/vnd.ionos.beat.buildwithionos-v1+json --ignore-stdin | jq -r .username)
 
 export SSHPASS=$password
 
@@ -18,5 +18,5 @@ if [[ $? -gt 0 ]] ; then
   exit 1
 fi
 
-http PUT https://api.buildwith.ionos.com/v1/projects/$PROJECT/git-repository/branches/$branch/hooks/DEPLOYED Authorization:"API-Key $API_KEY" --verify=no
+http PUT https://api.buildwith.ionos.com/v1/projects/$PROJECT/git-repository/branches/$branch/hooks/DEPLOYED Authorization:"API-Key $API_KEY"
 
