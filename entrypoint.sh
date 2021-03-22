c83798240af24f05f9e31657e5178ef7d009d53e
Robin Müller <robin.mueller@ionos.com>
2021-03-22 13:39:03 +0100

    URL encode slashes in the branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ remote_host=$2
 password=$(pwgen -s 30 1)
 
 branch=${GITHUB_REF/refs\/heads\//}
+branch=${branch/\//%2F}
 
 username=$(http POST https://${SERVICE_HOST}/v1/projects/$PROJECT/branches/$branch/users password=$password Authorization:"API-Key $API_KEY" Content-Type:application/json --ignore-stdin | jq -r .username)
 
